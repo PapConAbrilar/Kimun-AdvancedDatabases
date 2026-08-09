2fb9a25f168607770ab2c24fabe41ca5f63fb7a5
PapConAbrilar <benjapinto.bp@gmail.com>
2026-08-09 16:42:16 -0400

    Parche: Separar Tablas Globales en dos independientes

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,17 +151,14 @@ resource "aws_dynamodb_table" "kimun_data" {
     name = "PK"
     type = "S"
   }
-
   attribute {
     name = "SK"
     type = "S"
   }
-
   attribute {
     name = "GSI1PK"
     type = "S"
   }
-
   attribute {
     name = "GSI1SK"
     type = "S"
@@ -174,12 +171,45 @@ resource "aws_dynamodb_table" "kimun_data" {
     projection_type    = "ALL"
   }
 
-  # Configuración de Réplica (Tablas Globales - Nodo 2)
-  replica {
-    region_name = var.aws_region_secondary
+  tags = {
+    Environment = "Taller-Academico"
+    Region      = "us-east-1"
+  }
+}
+
+resource "aws_dynamodb_table" "kimun_data_replica" {
+  provider         = aws.replica
+  name             = var.dynamodb_table_name
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "PK"
+  range_key        = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+  attribute {
+    name = "GSI1PK"
+    type = "S"
+  }
+  attribute {
+    name = "GSI1SK"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "GSI1"
+    hash_key           = "GSI1PK"
+    range_key          = "GSI1SK"
+    projection_type    = "ALL"
   }
 
   tags = {
     Environment = "Taller-Academico"
+    Region      = "sa-east-1"
   }
 }
